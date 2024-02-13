97ecd6ca04457293ad8b2455cca440824d1ae549
Anant Shukla <accounts@anantshukla.com>
2024-02-13 06:10:24 -0500

    Time: 39 ms (38.91%) | Memory: 16.6 MB (80.12%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def recPow(b, p):
+            if b == 0: return 0
+            if p == 0: return 1
+            
+            res = recPow(b, p//2)
+            res = res * res
+
+            return res * b if p%2 else res
+
+
+        ans = recPow(x, abs(n))
+        
+        return 1/ans if(n < 0) else ans
+
+        
