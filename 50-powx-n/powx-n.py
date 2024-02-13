440198258ea6e8be2fb307d2b4137ece61728cb8
Anant Shukla <accounts@anantshukla.com>
2024-02-13 06:12:48 -0500

    Time: 38 ms (47.34%) | Memory: 16.6 MB (80.12%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -4,14 +4,11 @@ class Solution:
             if b == 0: return 0
             if p == 0: return 1
             
-            res = recPow(b, p//2)
-            res = res * res
-
+            res = recPow(b * b, p//2)
+            # res = res * res
             return res * b if p%2 else res
 
-
         ans = recPow(x, abs(n))
-        
         return 1/ans if(n < 0) else ans
 
         
