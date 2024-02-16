e9bed4622ddbc73bf299750da1d20ff997fb37ce
Anant Shukla <accounts@anantshukla.com>
2024-02-15 21:16:24 -0500

    Time: 730 ms (35.43%) | Memory: 27.4 MB (75.59%) - LeetSync

---
+++ 2342-minimum-average-difference/minimum-average-difference.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+        totalSum = sum(nums)
+
+        ansIdx, leftSum = 0, 0
+        minDiff = float('inf')
+        
+        for i in range(n):
+            leftSum += nums[i]
+            lAvg = math.floor(leftSum/(i+1))
+            rAvg = math.floor(totalSum - leftSum) // (n - i - 1) if (n - i - 1) !=0 else 0
+
+            currDiff = int(abs(rAvg - lAvg))
+
+            if currDiff < minDiff:
+                minDiff = currDiff
+                ansIdx = i
+        
+        return ansIdx
+
+        
