431252e3d4f3b8ad8e59fb194d9f165f3ee3b44d
Anant Shukla <accounts@anantshukla.com>
2024-02-23 01:05:34 -0500

    Time: 35 ms (61.07%) | Memory: 16.6 MB (77.78%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -9,9 +9,7 @@ class Solution:
         def recursiveCheck(t1, t2):
             if not t1 and not t2:
                 return True
-            if not t1 or not t2:
-                return False
-            if t1.val != t2.val:
+            if not t1 or not t2 or t1.val != t2.val:
                 return False
             
             return recursiveCheck(t1.left, t2.left) and recursiveCheck(t1.right, t2.right)
