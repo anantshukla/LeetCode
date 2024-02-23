0df25761b9f53c59cff903815ac11e4296dbc739
Anant Shukla <accounts@anantshukla.com>
2024-02-23 01:36:05 -0500

    Time: 34 ms (70.44%) | Memory: 16.6 MB (77.78%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,13 +6,37 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def recursiveCheck(t1, t2):
-            if not t1 and not t2:
-                return True
-            if not t1 or not t2 or t1.val != t2.val:
-                return False
-            
-            return recursiveCheck(t1.left, t2.left) and recursiveCheck(t1.right, t2.right)
+        q1, q2 = [p], [q]
         
-        return recursiveCheck(p, q)
-    
+        while q1 and q2:
+            t1, t2 = q1.pop(0), q2.pop(0)
+
+            if t1 == None and t2 == None:
+                continue
+            if (t1 == None and t2 != None) or (t1 != None and t2 == None):
+                return False
+            if t1.val != t2.val:
+                return False
+                
+            q1.append(t1.left)
+            q2.append(t2.left)
+            q1.append(t1.right)
+            q2.append(t2.right)
+
+        return not q1 and not q2
+            
+
+
+
+
+
+    # def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    #     def recursiveCheck(t1, t2):
+    #         if not t1 and not t2:
+    #             return True
+    #         if not t1 or not t2 or t1.val != t2.val:
+    #             return False
+            
+    #         return recursiveCheck(t1.left, t2.left) and recursiveCheck(t1.right, t2.right)
+        
+    #     return recursiveCheck(p, q)
