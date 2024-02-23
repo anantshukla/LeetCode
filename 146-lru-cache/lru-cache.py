2b8c04f210d5193aaa766ec3c49c7a43eb6f191c
Anant Shukla <accounts@anantshukla.com>
2024-02-23 02:54:48 -0500

    Time: 574 ms (60.41%) | Memory: 77.7 MB (83.47%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,18 +1,22 @@
-class LRUCache:
-    class Node:
-        def __init__(self, key, val, next, prev):
-            self.key, self.val = key, val
-            self.prev, self.next = prev, next
+class Node:
+    def __init__(self, key, val, next, prev):
+        self.key, self.val = key, val
+        self.prev, self.next = prev, next
 
-    def __init__(self, capacity: int):
-        self.hm = {}
-        self.maxCap = capacity
-        self.lru, self.mru = LRUCache.Node(0, 0, None, None), LRUCache.Node(0, 0, None, None)
-        self.mru.prev, self.lru.next = self.lru, self.mru
     
-    # L - LRU, R - MRU
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cacheHM = {}
+        self.maxCap = capacity
+        self.dummyLRU = Node(0, 0, None, None)
+        self.dummyMRU = Node(0, 0, None, None)
+        self.dummyLRU.next = self.dummyMRU
+        self.dummyMRU.prev = self.dummyLRU
+    
+    # L - LRU
+    # R - MRU
     def insert(self, node):
-        prev, nxt = self.mru.prev, self.mru
+        prev, nxt = self.dummyMRU.prev, self.dummyMRU
         node.prev, node.next = prev, nxt
         prev.next = nxt.prev = node
 
@@ -21,23 +25,22 @@ class LRUCache:
         prev.next, nxt.prev = nxt, prev
 
     def get(self, key: int) -> int:
-        if key not in self.hm:
+        if key not in self.cacheHM:
             return -1
-        curKey = self.hm[key]        
-        self.delete(curKey)
-        self.insert(curKey)
-        return self.hm[key].val
+        node = self.cacheHM[key]        
+        self.delete(node)
+        self.insert(node)
+        return self.cacheHM[key].val
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hm:
-            self.delete(self.hm[key])
+        if key in self.cacheHM:
+            self.delete(self.cacheHM[key])
 
-        self.hm[key] = LRUCache.Node(key, value, None, None)
-        self.insert(self.hm[key])
+        self.cacheHM[key] = Node(key, value, None, None)
+        self.insert(self.cacheHM[key])
 
-        if len(self.hm) > self.maxCap:
-            curLRU = self.lru.next
-            self.delete(curLRU)
-            self.hm.pop(curLRU.key)
-
+        if len(self.cacheHM) > self.maxCap:
+            lru = self.dummyLRU.next
+            self.delete(lru)
+            self.cacheHM.pop(lru.key)
