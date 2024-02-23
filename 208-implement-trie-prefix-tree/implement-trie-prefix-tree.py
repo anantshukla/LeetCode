4a34339e6b66748fc84048185d240d396c9e7c83
Anant Shukla <accounts@anantshukla.com>
2024-02-23 18:18:04 -0500

    Time: 134 ms (45.21%) | Memory: 30.9 MB (61.69%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -16,24 +16,24 @@ class Trie:
         node.endOfWord = True
         
 
-    # def search(self, word: str) -> bool:
-    #     node = self.root
-    #     for char in word:
-    #         if char not in node.nodeChars:
-    #             return False
-    #         node = node.nodeChars[char]
-    #     return node.endOfWord
-    
     def search(self, word: str) -> bool:
-        def recursiveSearch(substring, node):
-            if len(substring) == 0:
-                return node.endOfWord
-            
-            char = substring[0]
+        node = self.root
+        for char in word:
             if char not in node.nodeChars:
                 return False
-            return recursiveSearch(substring[1:], node.nodeChars[char])
-        return recursiveSearch(word, self.root)
+            node = node.nodeChars[char]
+        return node.endOfWord
+    
+    # def search(self, word: str) -> bool:
+    #     def recursiveSearch(substring, node):
+    #         if len(substring) == 0:
+    #             return node.endOfWord
+            
+    #         char = substring[0]
+    #         if char not in node.nodeChars:
+    #             return False
+    #         return recursiveSearch(substring[1:], node.nodeChars[char])
+    #     return recursiveSearch(word, self.root)
 
 
     def startsWith(self, prefix: str) -> bool:
