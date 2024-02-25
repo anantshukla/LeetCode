b813075b971c93fa3b824fe0bb08021c4ce42e62
Anant Shukla <accounts@anantshukla.com>
2024-02-25 12:31:46 -0500

    Time: 1144 ms (9.73%) | Memory: 35.8 MB (94.47%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -5,9 +5,8 @@ class Solution:
 
         for p, s in sorted(carPos)[::-1]:
             stack.append((target - p)/s)
+            if len(stack) > 1:
+                print(stack[-1], stack[-2])
             if len(stack) > 1 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
-
-
-        
