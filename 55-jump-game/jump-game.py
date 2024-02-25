fa15cca3ed20078e43e12c57dba5acf94ddf20e3
Anant Shukla <accounts@anantshukla.com>
2024-02-25 13:01:10 -0500

    Time: 371 ms (49.61%) | Memory: 17.7 MB (92.54%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -3,9 +3,8 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goalPtr = len(nums) - 1
         currPtr = goalPtr - 1
-        while currPtr >=0:
+        for currPtr in range(len(nums) - 1, -1, -1):
             if nums[currPtr] >= goalPtr - currPtr:
                 goalPtr = currPtr
-            currPtr -= 1
         
-        return (currPtr == -1 and goalPtr == 0)
+        return (currPtr == 0 and goalPtr == 0)
