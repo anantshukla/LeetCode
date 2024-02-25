74a47311e94112450cf188e78bb7896dfabdceb7
Anant Shukla <accounts@anantshukla.com>
2024-02-24 21:55:37 -0500

    Time: 69 ms (73.80%) | Memory: 20.2 MB (46.67%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -2,11 +2,10 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.sumArray = [0] * len(nums)
-        tempSum = 0
-        for i, val in enumerate(nums):
-            tempSum += val
-            self.sumArray[i] = tempSum
-
+        if len(nums) > 0:
+            self.sumArray[0] = nums[0]
+        for i in range(1, len(nums)):
+            self.sumArray[i] += self.sumArray[i - 1] + nums[i]
 
     def sumRange(self, left: int, right: int) -> int:
         lVal = self.sumArray[left - 1] if left > 0 else 0
