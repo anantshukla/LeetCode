b6c75f8c785bfd5ef0640c3f8ec13f68b0e015ac
Anant Shukla <accounts@anantshukla.com>
2024-02-24 23:13:56 -0500

    Time: 40 ms (27.28%) | Memory: 16.6 MB (48.40%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack, ptr = [], 0
+        while ptr < len(s):
+            if s[ptr] != ']':
+                stack.append(s[ptr])
+            else:
+                outStr = ""
+                while stack[-1] != '[':
+                    outStr = stack.pop() + outStr
+                stack.pop()
+
+                multiplicationFactor = ""
+                while stack and stack[-1].isdigit():
+                    multiplicationFactor = stack.pop() + multiplicationFactor
+                multiplicationFactor = int(multiplicationFactor)
+                outStr = outStr * multiplicationFactor
+                stack.append(outStr)
+            ptr += 1
+        outStr = ""
+        while stack:
+            outStr = stack.pop() + outStr
+        return outStr
