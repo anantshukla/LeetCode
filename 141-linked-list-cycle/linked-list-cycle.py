aacd0434b07bb7c204c3e6c465eb5b4dbfb8afc1
Anant Shukla <accounts@anantshukla.com>
2024-02-27 01:04:58 -0500

    Time: 46 ms (57.91%) | Memory: 19 MB (83.16%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -5,20 +5,29 @@
 #         self.next = None
 
 class Solution:
-    
+    # TC: O(n), SC: O(1)
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        ptr1, ptr2 = head, head
+        rear, forward = head, head
+        
+        while rear and forward and forward.next:
+            rear = rear.next
+            forward = forward.next.next
+
+            if forward == rear:
+                return True
+        return False
+
 
 
     # TC: O(n), SC: O(n)
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hs = set()
-        ptr = head
-        while ptr:
-            if ptr in hs:
-                return True
-            hs.add(ptr)
-            ptr = ptr.next
-        return False
+    # def hasCycle(self, head: Optional[ListNode]) -> bool:
+    #     hs = set()
+    #     ptr = head
+    #     while ptr:
+    #         if ptr in hs:
+    #             return True
+    #         hs.add(ptr)
+    #         ptr = ptr.next
+    #     return False
 
         
