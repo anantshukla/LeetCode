49c8bdc7ac151f7b8e68582bb03d2f9a1fe65736
Anant Shukla <accounts@anantshukla.com>
2024-02-29 00:48:37 -0500

    Time: 53 ms (40.33%) | Memory: 17 MB (77.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,14 +4,7 @@ class Solution:
             return False
         hMapS, hMapT = {}, {}
         for cs, ct in zip(s, t):
-            if cs in hMapS:
-                hMapS[cs] += 1
-            else:
-                hMapS[cs] = 1
-            
-            if ct in hMapT:
-                hMapT[ct] += 1
-            else:
-                hMapT[ct] = 1
+            hMapS[cs] = 1 + hMapS.get(cs, 0)
+            hMapT[ct] = 1 + hMapT.get(ct, 0)
 
         return hMapS == hMapT
