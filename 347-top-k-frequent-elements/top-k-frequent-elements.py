cd8150554fea8bb5a3b56e6c90c1eb2bfbf07130
Anant Shukla <accounts@anantshukla.com>
2024-02-29 18:43:11 -0500

    Time: 94 ms (37.36%) | Memory: 22 MB (23.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         numFreqMap = {}
+        buckets = [[] for i in range(len(nums) + 1)]
         for num in nums:
             numFreqMap[num] = 1 + numFreqMap.get(num, 0)
-
-        heapList = [(-freq, num) for num, freq in numFreqMap.items()]
-        heapq.heapify(heapList)
+        # O(n)
+        for num, count in numFreqMap.items():
+            buckets[count].append(num)
         res = []
-        for _ in range(k):
-            _, num = heapq.heappop(heapList)
-            res.append(num)
-        return res
+        for i in range(len(buckets) - 1, 0, -1):
+            for n in buckets[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        # # O(n * k.log(n))
+        # heapList = [(-freq, num) for num, freq in numFreqMap.items()]
+        # heapq.heapify(heapList)
+        # res = []
+        # for _ in range(k):
+        #     _, num = heapq.heappop(heapList)
+        #     res.append(num)
+        # return res
