dda1b50c2d7cb38d281d579d5ab9c7ed65bc8bc8
Anant Shukla <accounts@anantshukla.com>
2024-02-29 18:48:06 -0500

    Time: 101 ms (22.27%) | Memory: 22.4 MB (9.64%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,12 @@
 class Solution:
+    # TC: O(m * n), where m is the number of elements, n is the length of string
+    # SC: O(m * 26)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hMap = collections.defaultdict(list)
         for word in strs:
             charArr = [0] * 26
             for char in word:
                 charArr[ord(char) - ord('a')] += 1
-            # charStr = ''.join(map(str, charArr))
+            # Tuple is immutable and therefore can be hashed
             hMap[tuple(charArr)].append(word)
         return hMap.values()
