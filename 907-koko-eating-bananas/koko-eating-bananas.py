7cd185c204b205acfed1b4941f524642ff6c50ac
Anant Shukla <accounts@anantshukla.com>
2024-03-04 17:03:13 -0500

    Time: 327 ms (12.37%) | Memory: 18.1 MB (69.37%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -11,7 +11,7 @@ class Solution:
             time = 0
             for pile in piles:
                 time += math.ceil(float(pile) / mid)
-                # if time > h: break
+                if time > h: break
             
             if time <= h:
                 minSpeed = mid
