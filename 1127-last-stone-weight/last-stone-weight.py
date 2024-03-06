6555116fbf5af32d0e1854b94d9acb4bf7a45d30
Anant Shukla <accounts@anantshukla.com>
2024-03-06 15:27:42 -0500

    Time: 36 ms (55.97%) | Memory: 16.7 MB (29.21%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -6,8 +6,8 @@ class Solution:
             stone_1 = heapq.heappop(stones)
             stone_2 = heapq.heappop(stones)
             print(stone_1, stone_2)
-            if stone_1 - stone_2 != 0:
-                heapq.heappush(stones, -abs(stone_1 - stone_2))
+            if stone_1 - stone_2 < 0:
+                heapq.heappush(stones, stone_1 - stone_2)
         if not stones:
             return 0
         return abs(heapq.heappop(stones))
