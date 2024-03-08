339e8214ba493896e3e17cbd801bb5e62cca5efd
Anant Shukla <accounts@anantshukla.com>
2024-03-08 17:03:48 -0500

    Time: 339 ms (93.75%) | Memory: 17.8 MB (89.97%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -2,7 +2,7 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
         canReachPtr = n - 1
-        for i in range(n - 1, -1, -1):
+        for i in range(n - 2, -1, -1):
             if nums[i] + i >= canReachPtr:
                 canReachPtr = i
         
