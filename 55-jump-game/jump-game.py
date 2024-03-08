3f285f0355ebb927c4563ebaaffb4b9451af51a8
Anant Shukla <accounts@anantshukla.com>
2024-03-08 16:48:26 -0500

    Time: 344 ms (86.14%) | Memory: 17.8 MB (89.97%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,20 +1,9 @@
 class Solution:
-    # Greedy - TC: O(n), SC: O(1)
-    # def canJump(self, nums: List[int]) -> bool:
-    #     goalPtr = len(nums) - 1
-    #     currPtr = goalPtr - 1
-    #     for currPtr in range(len(nums) - 1, -1, -1):
-    #         if nums[currPtr] >= goalPtr - currPtr:
-    #             goalPtr = currPtr
-    #     return goalPtr == 0
-    
-    # DP - TC: O(n^2), SC: O(n)
     def canJump(self, nums: List[int]) -> bool:
-        dp = [False] * len(nums)
-        dp[0] = True # B'cuz we start at 0, so we know it is accessible
-        for i in range(1, len(nums)):
-            for j in range(i - 1, -1, -1):
-                if nums[j] >= i - j and dp[j]:
-                    dp[i] = True
-                    break
-        return dp[-1]
+        n = len(nums)
+        canReachPtr = n - 1
+        for i in range(n - 1, -1, -1):
+            if nums[i] + i >= canReachPtr:
+                canReachPtr = i
+        
+        return canReachPtr == 0
