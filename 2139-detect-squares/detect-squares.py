4b7fdc5910eefd2c4d8def4eabb26d25822ecfe4
Anant Shukla <accounts@anantshukla.com>
2024-03-10 15:20:06 -0400

    Time: 249 ms (69.50%) | Memory: 18.2 MB (96.17%) - LeetSync

--- 2139-detect-squares/detect-squares.py
+++ 2139-detect-squares/detect-squares.py
@@ -1,18 +1,16 @@
 class DetectSquares:
     def __init__(self):
         self.pointsCount = defaultdict(int)
-        self.points = []
 
     def add(self, point: List[int]) -> None:
         self.pointsCount[tuple(point)] += 1
-        self.points.append(point)
 
     def count(self, point: List[int]) -> int:
         ans = 0
         px, py = point
         for (x, y), count in self.pointsCount.items():
             # Checking if the iter point forms a diagonal with given point
-            if x != px and y!= py and abs(x - px) == abs(y - py) and (px, y) in self.pointsCount and (x, py) in self.pointsCount:
+            if x != px and y != py and abs(x - px) == abs(y - py) and (px, y) in self.pointsCount and (x, py) in self.pointsCount:
                 ans += count * self.pointsCount[(px, y)] * self.pointsCount[(x, py)]
         return ans
 
