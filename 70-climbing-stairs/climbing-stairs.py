86b9e0e1cfd580bff8c91e7ba19a8c16de1a2f96
Anant Shukla <accounts@anantshukla.com>
2024-03-13 18:26:56 -0400

    Time: 36 ms (48.47%) | Memory: 16.5 MB (61.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,10 +2,20 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        arr = [1] * (n + 1)
+        onePrev, twoPrev = 1, 1
+        curr = 0
         for i in range(2, n + 1):
-            arr[i] = arr[i - 1] + arr[i - 2]
-        return arr[n]
+            curr = onePrev + twoPrev
+            twoPrev = onePrev
+            onePrev = curr
+        return curr
+
+        # if n == 1:
+        #     return 1
+        # arr = [1] * (n + 1)
+        # for i in range(2, n + 1):
+        #     arr[i] = arr[i - 1] + arr[i - 2]
+        # return arr[n]
 
         # def numOfWays(remaining):
         #     if remaining == 1 or remaining == 0:
