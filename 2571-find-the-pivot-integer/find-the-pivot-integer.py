5828bd35fda97f2dc41f55df4431eaf792d05e2b
Anant Shukla <accounts@anantshukla.com>
2024-03-13 20:13:29 -0400

    Time: 49 ms (51.93%) | Memory: 16.6 MB (22.77%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -5,18 +5,14 @@ class Solution:
         for i in range(n):
             currSum += i + 1
             sumArray[i] = currSum
-        print(sumArray)
         currSum = 0
         for i in range(n - 1, -1, -1):
             currSum += i + 1
             sumArray[i] -= currSum
-
-        print(sumArray)
         
         for idx, val in enumerate(sumArray):
             if val == 0:
                 return idx + 1
-        print(sumArray)
         return -1
 
         
