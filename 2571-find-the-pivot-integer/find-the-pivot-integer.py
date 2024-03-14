ddd0995707a88703a6a96c843a2a51c305a302f1
Anant Shukla <accounts@anantshukla.com>
2024-03-13 20:28:27 -0400

    Time: 61 ms (32.74%) | Memory: 16.5 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,18 +1,16 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        sumArray = [0] * n
-        currSum = 0
-        for i in range(n):
-            currSum += i + 1
-            sumArray[i] = currSum
-        currSum = 0
-        for i in range(n - 1, -1, -1):
-            currSum += i + 1
-            sumArray[i] -= currSum
+        # Using Sum of Arithematic Progression
+        def sumOfnNums(n):
+            return (n * (n + 1)) / 2
+
+        totalSum = sumOfnNums(n)
         
-        for idx, val in enumerate(sumArray):
-            if val == 0:
-                return idx + 1
+        for i in range(1, n + 1):
+            left = sumOfnNums(i)
+            right = totalSum - sumOfnNums(i) + i
+            if left == right:
+                return i
         return -1
 
         
