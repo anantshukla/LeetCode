00d20af1e0eb786b3dca5d2f17151d5ac5bf0eaf
Anant Shukla <accounts@anantshukla.com>
2024-03-15 09:35:59 -0400

    Time: 615 ms (32.15%) | Memory: 21.6 MB (45.11%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,14 +1,24 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        incoming = defaultdict(int)
-        outgoing = defaultdict(int)
+        deltaEdges = defaultdict(int) # incoming - outgoing
 
         for out, inc in trust:
-            outgoing[out] += 1
-            incoming[inc] += 1
+            deltaEdges[inc] += 1
+            deltaEdges[out] -= 1
         
         for i in range(1, n + 1):
-            if outgoing[i] == 0 and incoming[i] == (n - 1):
+            if deltaEdges[i] == (n - 1):
                 return i
         return -1
+        # incoming = defaultdict(int)
+        # outgoing = defaultdict(int)
+
+        # for out, inc in trust:
+        #     outgoing[out] += 1
+        #     incoming[inc] += 1
+        
+        # for i in range(1, n + 1):
+        #     if outgoing[i] == 0 and incoming[i] == (n - 1):
+        #         return i
+        # return -1
         
