e15f3a21e0e644a474687e049365689509659482
Anant Shukla <accounts@anantshukla.com>
2024-03-15 19:20:55 -0400

    Time: 30 ms (87.16%) | Memory: 16.7 MB (14.13%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -7,35 +7,22 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         q1, q2 = [p], [q]
-        
         while q1 and q2:
             t1, t2 = q1.pop(0), q2.pop(0)
 
             if not t1 and not t2:
                 continue
+            
             if (not t1 and t2) or (t1 and not t2) or t1.val != t2.val:
                 return False
             
             q1.append(t1.left)
             q1.append(t1.right)
-
             q2.append(t2.left)
             q2.append(t2.right)
-
-        return not q1 and not q2
-            
-
-
-
-
-
-    # def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    #     def recursiveCheck(t1, t2):
-    #         if not t1 and not t2:
-    #             return True
-    #         if not t1 or not t2 or t1.val != t2.val:
-    #             return False
-            
-    #         return recursiveCheck(t1.left, t2.left) and recursiveCheck(t1.right, t2.right)
         
-    #     return recursiveCheck(p, q)
+        return not q1 and not q2
+
+
+
+        
