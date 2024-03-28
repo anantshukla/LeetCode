e46788113abee2b88a78ce819130322451a641a7
Anant Shukla <accounts@anantshukla.com>
2024-03-27 22:02:23 -0400

    Time: 75 ms (11.57%) | Memory: 43 MB (12.19%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cs
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cs
@@ -1,11 +1,5 @@
 public class Solution {
     public int NumberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int n = 0;
-        foreach(int hour in hours) {
-            if (hour >= target) {
-                n += 1;
-            }
-        }
-        return n;
+        return hours.Where(s => s >= target).Count();
     }
 }
