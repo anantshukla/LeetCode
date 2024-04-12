9fcb28ddfc48bb9cf61164da45f0889305838236
Anant Shukla <accounts@anantshukla.com>
2024-04-12 08:03:05 -0400

    Time: 929 ms (29.06%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,14 +1,6 @@
 # Write your MySQL query statement below
-SELECT
-    num
-FROM
-    MyNumbers
-GROUP BY
-    num
-HAVING
-    COUNT(num) = 1
-UNION ALL
-    SELECT NULL
-ORDER BY
-    num DESC
-LIMIT 1;
+-- SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1
+-- UNION ALL SELECT NULL
+-- ORDER BY num DESC LIMIT 1;
+
+SELECT MAX(num) as num FROM MyNumbers WHERE num IN (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1)
