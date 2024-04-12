9dfb57e2f71a35ea3ed9f0fff02c857488719235
Anant Shukla <accounts@anantshukla.com>
2024-04-12 07:56:18 -0400

    Time: 817 ms (50.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT
+    num
+FROM
+    MyNumbers
+GROUP BY
+    num
+HAVING
+    COUNT(num) = 1
+UNION ALL
+    SELECT NULL
+ORDER BY
+    num DESC
+LIMIT 1;
